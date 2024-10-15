5ad51964f65344f57dbbb9f5c613e29a068beb6e
21Breaker <157035437+21Breaker@users.noreply.github.com>
2024-10-15 00:20:16 -0400

    Update SalesTransaction (Abstract Class).java

--- SalesTransaction (Abstract Class).java
+++ SalesTransaction (Abstract Class).java
@@ -1,8 +1,8 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
-/**
- * Abstract class representing a sales transaction.
- */
 public abstract class SalesTransaction {
     protected String transactionID;
     protected LocalDate date;
@@ -10,14 +10,6 @@ public abstract class SalesTransaction {
     protected String customerName;
     protected String productID;
 
-    /**
-     * Constructor for SalesTransaction.
-     * @param transactionID Unique ID for the transaction.
-     * @param date Date of the transaction.
-     * @param amount Amount of the transaction.
-     * @param customerName Name of the customer.
-     * @param productID ID of the product.
-     */
     public SalesTransaction(String transactionID, LocalDate date, double amount, String customerName, String productID) {
         this.transactionID = transactionID;
         this.date = date;
@@ -26,13 +18,25 @@ public abstract class SalesTransaction {
         this.productID = productID;
     }
 
-    /**
-     * Abstract method to calculate the total amount of the transaction.
-     */
     public abstract void calculateTotal();
 
-    /**
-     * Abstract method to validate the transaction.
-     */
     public abstract void validateTransaction();
+
+    public void saveToDatabase() {
+        Connection connection = JDBC.getConnection();
+        if (connection != null) {
+            try {
+                String query = "INSERT INTO sales_transactions (transactionID, date, amount, customerName, productID) VALUES (?, ?, ?, ?, ?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, this.transactionID);
+                preparedStatement.setDate(2, java.sql.Date.valueOf(this.date));
+                preparedStatement.setDouble(3, this.amount);
+                preparedStatement.setString(4, this.customerName);
+                preparedStatement.setString(5, this.productID);
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
